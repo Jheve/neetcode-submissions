bf54d3664fe5445e36a292ae5f1c626574534a83
Jheve <118742881+Jheve@users.noreply.github.com>
2026-05-14 15:41:21 -0400

    Add: word-break - submission-5

---
+++ Data Structures & Algorithms/word-break/submission-5.py
@@ -0,0 +1,18 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        dp = [False] * (len(s) + 1)   # set up dp array 
+        dp[len(s)] = True             # base case -> empty suffix  
+
+        # iterate from right to left
+        for i in range(len(s) - 1, -1, -1):
+            for word in wordDict:
+                # check if word slice is within bounds and matches target word
+                if (i + len(word)) <= len(s) and s[i : i + len(word)] == word:
+                    dp[i] = dp[i + len(word)]
+                # early break -> if one word meets dp requirement, no need to check anymore
+                if dp[i]:
+                    break
+
+        return dp[0]
+        
+        return dfs(0)
